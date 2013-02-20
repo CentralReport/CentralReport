0d60b364784828d20b778b5e3b5f174f346f4b87
Charles-Emmanuel <che.camus@gmail.com>
2013-02-20 21:50:40 +0100

    Improved log module

--- centralreport/cr/log.py
+++ centralreport/cr/log.py
@@ -2,7 +2,7 @@
 
 """
     CentralReport - Log module
-        Contains log function to work with Python logging modules
+        Contains log functions to work with native Python logging modules
 
     https://github.com/miniche/CentralReport/
 """
@@ -12,62 +12,83 @@ import logging.handlers
 import sys
 
 # Our custom logger object. Initialized on the first use.
-crLogger = None
+cr_logger = None
 debug_mode_enabled = False
 
 
-def getCrLogger():
+def get_cr_logger():
     """
         Gets the Logger object. This function initializes it if it's the first call.
     """
 
-    global crLogger
+    global cr_logger
     global debug_mode_enabled
 
-    if crLogger is None:
-        # Using python rotating log function
-        crLogger = logging.getLogger()
+    if cr_logger is None:
+        # Using native python functions to manage logs
+        cr_logger = logging.getLogger()
 
-        logLevel = logging.INFO if debug_mode_enabled is False else logging.DEBUG
-        crLogger.setLevel(logLevel)
+        log_level = logging.INFO if debug_mode_enabled is False else logging.DEBUG
+        cr_logger.setLevel(log_level)
 
         # In debug mode, log file must be in "/tmp", due to system permissions.
-        logFilename = '/var/log/centralreport.log' if debug_mode_enabled is False else '/tmp/centralreport.log'
+        log_filename = '/var/log/centralreport.log' if debug_mode_enabled is False else '/tmp/centralreport.log'
 
-        # Max size per log file: 5 MB (1024 * 1024 * 5). Max log files: 2.
-        customRotatingFileHandler = logging.handlers.RotatingFileHandler(logFilename, maxBytes=5242880, backupCount=2)
-        customRotatingFileHandler.setFormatter(logging.Formatter(fmt='%(levelname)s \t %(asctime)s \t %(message)s',
-                                                                 datefmt='%m/%d/%Y %I:%M:%S'))
+        # Max size per log file: 5 MB (1024 * 1024 * 5). Number of log files will be kept for archive: 2.
+        custom_rotating_fileHandler = logging.handlers.RotatingFileHandler(log_filename,
+                                                                           maxBytes=5242880,
+                                                                           backupCount=2)
 
-        crLogger.addHandler(customRotatingFileHandler)
+        custom_rotating_fileHandler.setFormatter(logging.Formatter(fmt='%(levelname)s \t %(asctime)s \t %(message)s',
+                                                                   datefmt='%m/%d/%Y %I:%M:%S'))
+
+        cr_logger.addHandler(custom_rotating_fileHandler)
 
         if debug_mode_enabled:
             # In debug mode, we display logs on the standard output too.
-            customConsoleHandler = logging.StreamHandler(stream=sys.stdout)
-            customConsoleHandler.setLevel(logging.DEBUG)
-            customConsoleHandler.setFormatter(logging.Formatter(fmt='%(levelname)s \t %(asctime)s \t %(message)s',
-                                                                datefmt='%m/%d/%Y %I:%M:%S'))
+            custom_console_handler = logging.StreamHandler(stream=sys.stdout)
+            custom_console_handler.setLevel(logging.DEBUG)
+            custom_console_handler.setFormatter(logging.Formatter(fmt='%(levelname)s \t %(asctime)s \t %(message)s',
+                                                                  datefmt='%m/%d/%Y %I:%M:%S'))
 
-            crLogger.addHandler(customConsoleHandler)
+            cr_logger.addHandler(custom_console_handler)
 
-    return crLogger
+    return cr_logger
 
 
 def writeDebug(text):
-    getCrLogger().debug(text)
+    """
+        Writes a debug message. Only useful for testing and debugging purposes.
+    """
+    get_cr_logger().debug(text)
 
 
 def writeInfo(text):
-    getCrLogger().info(text)
+    """
+        Writes a standard message. Will be written in the current log file.
+    """
+    get_cr_logger().info(text)
 
 
 def writeError(text):
-    getCrLogger().error(text)
+    """
+        Writes an error message: an anormal situation, but non-critical.
+        Will be written in the current log file.
+    """
+    get_cr_logger().error(text)
 
 
 def writeCritical(text):
-    getCrLogger().critical(text)
+    """
+        Writes a critical error. This error requires to stop the current action.
+        Will be written in the current log file.
+    """
+    get_cr_logger().critical(text)
 
 
 def writeFatal(text):
-    getCrLogger().fatal(text)
+    """
+        Writes a fatal error. This error requires to stop current application.
+        Will be written in the current log file.
+    """
+    get_cr_logger().fatal(text)
