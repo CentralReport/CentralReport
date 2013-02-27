fa63ef938f7800806ec886f1d05a11702abd22ea
Guillaume <dasilva.guillaume@live.fr>
2013-02-27 14:35:35 +0100

    Update tools/install_dev_tools.sh

--- tools/install_dev_tools.sh
+++ tools/install_dev_tools.sh
@@ -17,7 +17,7 @@ source ../bash/functions.inc.sh
 #=============================================================================
 # List of library to install
 # Can be updated as your needs
-declare -a LISTE='([0]="CherrezyPy" [1]="Jinja2" [2]="Routes")'
+declare -a LISTE='([0]="CherryPy" [1]="Jinja2" [2]="Routes")'
 #=============================================================================
 
 printLightBox blue "------------------------------------------"
