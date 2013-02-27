707eee8c76fabce19c7b087970fff484731aeb94
Guimove <dasilva.guillaume@live.fr>
2013-02-27 14:28:46 +0100

    Refactor install dev tools for easier usage

--- tools/install_dev_tools.sh
+++ tools/install_dev_tools.sh
@@ -14,6 +14,12 @@ source ../bash/vars.sh
 source ../bash/log.inc.sh
 source ../bash/functions.inc.sh
 
+#=============================================================================
+# List of library to install
+# Can be updated as your needs
+declare -a LISTE='([0]="CherrezyPy" [1]="Jinja2" [2]="Routes")'
+#=============================================================================
+
 printLightBox blue "------------------------------------------"
 printLightBox blue "CentralReport dev tools installer"
 printLightBox blue "------------------------------------------"
@@ -33,32 +39,18 @@ if [ $? -ne 0 ]; then
     exit
 fi
 
-# First, we install CherryPy...
-displayAndExec "Installing CherryPy..." sudo easy_install CherryPy
-RETURN_CODE="$?"
-if [ ${RETURN_CODE} -ne 0 ]; then
-    printLightBox red ${RETURN_CODE}
-    exit
-fi
-
-# Then, installing Jinja2 Templates...
-displayAndExec "Installing Jinja 2..." sudo easy_install Jinja2
-RETURN_CODE="$?"
-if [ ${RETURN_CODE} -ne 0 ]; then
-    printLightBox red ${RETURN_CODE}
-    exit
-fi
-
-# Finally, installing Routes library...
-displayAndExec "Installing Routes..." sudo easy_install routes
-RETURN_CODE="$?"
-if [ ${RETURN_CODE} -ne 0 ]; then
-    printLightBox red ${RETURN_CODE}
-    exit
-fi
+for i in ${!LISTE[*]}
+do
+    displayAndExec "Installing ${LISTE[${i}]}..." sudo easy_install ${LISTE[${i}]}
+    RETURN_CODE="$?"
+    if [ ${RETURN_CODE} -ne 0 ]; then
+        printLightBox red "Woops... Something went wrong with the installation of ${LISTE[${i}]}"
+        printLightBox red "Read the log file in ${ERROR_FILE} for further informations"
+        sudo -k
+        exit
+    fi
+done
 
 sudo -k
-
-echo " --- End of the program ---"
 
 exit 0
